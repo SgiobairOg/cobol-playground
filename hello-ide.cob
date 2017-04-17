911675243144dc42aaaa815a49b45380af5e9015
Jason Wilson <jason@wilsons.io>
2017-04-17 17:31:03 -0400

    File Name Refactor

---
+++ hello-ide.cob
@@ -0,0 +1,16 @@
+      ******************************************************************
+      * Author: Jason Wilson <jason@wilsons.io>
+      * Date: 04/17/2017
+      * Purpose: Trying out the OpenCobolIDE 4.7.4
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLO-IDE.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY FUNCTION MODULE-ID.
+            STOP RUN.
+       END PROGRAM HELLO-IDE.
